eef794445ee8d8203468bf697605dd9f8e206934
Rayane <rchaabane20@gmail.com>
2023-08-21 19:06:38 +0200

    changement des fichiers

--- script/script.js
+++ script/script.js
@@ -3,100 +3,121 @@ const radioButtons = document.querySelectorAll('.carrousel-buttons input[type="r
 const title = document.getElementById("title");
 const btnAuto = document.getElementById('autoplay-toggle');
 const textBtn = document.getElementById('autoplay-toggle');
-let autoplayInterval; // Variable pour stocker l'intervalle de lecture automatique
-let autoplayEnabled = false; // Indicateur de lecture automatique activée/désactivée
 const carrouselSlides = document.querySelector('.carrousel-slides');
 const prevButton = document.querySelector('.prev');
 const nextButton = document.querySelector('.next');
+let autoplayInterval;
+let autoplayEnabled = false;
 
+// Initialisation du carrousel
+function initializeCarousel() {
+  setInitialTitleAndButton();
+  addEventListeners();
+  showSlide(0);
+}
 
+// Mise à jour du titre et du bouton automatique initial
+function setInitialTitleAndButton() {
+  title.textContent = "À la Découverte de l'Exceptionnel : Les 7 Merveilles du Monde Ancien";
+  title.style.color = "#2f455b";
+  textBtn.textContent = "Activer la lecture automatique";
+}
 
-// Mise à jour du titre initial
-title.textContent = "À la Découverte de l'Exceptionnel : Les 7 Merveilles du Monde Ancien";
-title.style.color = "#2f455b";
-textBtn.textContent = "Activer la lecture automatique";
+// Ajout des écouteurs d'événements
+function addEventListeners() {
+  prevButton.addEventListener('click', prevSlide);
+  nextButton.addEventListener('click', nextSlide);
+  btnAuto.addEventListener('click', toggleAutoplay);
 
-
-// Écouteur d'événement pour le bouton précédent
-prevButton.addEventListener('click', prevSlide);
-
-// Écouteur d'événement pour le bouton suivant
-nextButton.addEventListener('click', nextSlide);
-
-// Écouteur d'événement pour le bouton de lecture automatique
-btnAuto.addEventListener('click', toggleAutoplay);
-
-
-// Fonction pour afficher la diapositive sélectionnée
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    if (i === index) {
-      slide.classList.add('active');  // Ajoute la classe "active" pour afficher la diapositive
-    } else {
-      slide.classList.remove('active'); // Supprime la classe "active" pour masquer la diapositive
-    }
+  radioButtons.forEach((button) => {
+    button.addEventListener('change', (event) => {
+      const selectedValue = parseInt(event.target.value);
+      showSlide(selectedValue);
+    });
   });
 }
 
-// Écouteur d'événement pour les boutons radio
-radioButtons.forEach((button) => {
-  button.addEventListener('change', (event) => {
-    const selectedValue = parseInt(event.target.value);
-    showSlide(selectedValue);
+// Afficher la diapositive sélectionnée
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
   });
-});
 
-// Initialisation : Activer le premier bouton radio et afficher la première diapositive
-radioButtons[0].checked = true;
-showSlide(0);
+  updateCopyright(index);
+  updateRadioButtonsState(index);
+}
+
+// Passer à la diapositive précédente
+function prevSlide() {
+  const currentSlide = document.querySelector('.carrousel-slides img.active');
+  const prevSlide = currentSlide.previousElementSibling || slides[slides.length - 1];
+  
+  currentSlide.classList.remove('active');
+  prevSlide.classList.add('active');
+  
+  const currentIndex = Array.from(slides).indexOf(prevSlide);
+  updateRadioButtonsState(currentIndex);
+  updateCopyright(currentIndex);
+}
+
+// Passer à la diapositive suivante
+function nextSlide() {
+  // Sélectionne l'élément image actuellement actif
+  const currentSlide = document.querySelector('.carrousel-slides img.active');
+
+  // Sélectionne l'élément image suivant de la liste, ou le premier élément s'il n'y en a pas de suivant
+  const nextSlide = currentSlide.nextElementSibling || slides[0];
+  
+  // Masque la diapositive actuelle en supprimant la classe "active"
+  currentSlide.classList.remove('active');
+
+  // Affiche la diapositive suivante en ajoutant la classe "active"
+  nextSlide.classList.add('active');
+  
+  // Met à jour l'état des boutons radio pour refléter la nouvelle diapositive affichée
+  const currentIndex = Array.from(slides).indexOf(nextSlide);
+  updateRadioButtonsState(currentIndex);
+
+  // Met à jour le contenu du droit d'auteur pour la nouvelle diapositive
+  updateCopyright(currentIndex);
+
+  // Si la diapositive suivante est la première diapositive, réinitialise l'intervalle de lecture automatique
+  if (nextSlide === slides[0]) {
+    clearInterval(autoplayInterval);
+    autoplayInterval = setInterval(nextSlide, 1500);
+  }
+}
 
 
 
-// Fonction pour basculer la lecture automatique
+// Bascule de la lecture automatique
 function toggleAutoplay() {
   if (autoplayEnabled) {
-    clearInterval(autoplayInterval); // Arrête la lecture automatique
+    clearInterval(autoplayInterval);
     autoplayEnabled = false;
     btnAuto.setAttribute('data-autoplay', 'off');
     textBtn.textContent = "Activer la lecture automatique";
   } else {
-    autoplayInterval = setInterval(nextSlide, 1500); // Change de diapositive toutes les 5 secondes
+    radioButtons[0].checked = true;
+    showSlide(0);
+    
+    autoplayInterval = setInterval(nextSlide, 1500);
     autoplayEnabled = true;
     btnAuto.setAttribute('data-autoplay', 'on');
     btnAuto.style.fontFamily = "Verdana";
     textBtn.style.fontSize = "28px";
     textBtn.textContent = "Lecture automatique en cours";
   }
+}
 
-  // Mettre à jour le bouton radio sélectionné pour refléter la nouvelle diapositive affichée
-  const currentIndex = Array.from(slides).indexOf(nextSlide);
-  radioButtons.forEach((button, index) => {
-    button.checked = index === currentIndex;
+// Fonction pour mettre à jour l'état des boutons radio en fonction de la diapositive affichée
+function updateRadioButtonsState(index) {
+  radioButtons.forEach((button, buttonIndex) => {
+    button.checked = buttonIndex === index;
   });
 }
 
-// Fonction pour passer à la diapositive précédente
-function prevSlide() {
-  // Sélectionne l'élément image actuellement actif
-  const currentSlide = document.querySelector('.carrousel-slides img.active');
-
-  // Sélectionne l'élément image précédent de la liste, ou le dernier élément s'il n'y en a pas de précédent
-  const prevSlide = currentSlide.previousElementSibling || slides[slides.length - 1];
-
-  // Supprime la classe "active" de l'image actuelle pour la masquer
-  currentSlide.classList.remove('active');
-
-  // Ajoute la classe "active" à l'image précédente pour l'afficher
-  prevSlide.classList.add('active');
-
-  // Mettre à jour le bouton radio sélectionné pour refléter la nouvelle diapositive affichée
-  const currentIndex = Array.from(slides).indexOf(prevSlide);
-  radioButtons.forEach((button, index) => {
-    button.checked = index === currentIndex;
-  });
-}
-
-// Fonction pour mettre à jour le contenu du droit d'auteur
+// Mettre à jour le contenu du droit d'auteur
 function updateCopyright(index) {
   const copyrights = [
     "Le Machu Picchu, Cuzco (Pérou) © Sergi Reboredo",
@@ -114,36 +135,5 @@ function updateCopyright(index) {
   }
 }
 
-// Fonction pour passer à la diapositive suivante
-function nextSlide() {
-  // Sélectionne l'élément image actuellement actif
-  const currentSlide = document.querySelector('.carrousel-slides img.active');
-
-  if (currentSlide) {
-
-    // Sélectionne l'élément image suivant de la liste, ou le premier élément s'il n'y en a pas de suivant
-    const nextSlide = currentSlide.nextElementSibling || slides[0];
-
-  // Vérifie si une diapositive suivante existe
-  if (nextSlide) {
-    // Supprime la classe "active" de l'image actuelle pour la masquer
-    currentSlide.classList.remove('active');
-
-    // Ajoute la classe "active" à l'image suivante pour l'afficher
-    nextSlide.classList.add('active');
-
-    // Mettre à jour le contenu du droit d'auteur
-    const currentIndex = Array.from(slides).indexOf(nextSlide);
-    updateCopyright(currentIndex);
-
-    // Mettre à jour le bouton radio sélectionné pour refléter la nouvelle diapositive affichée
-    const currentIndexRadio = Array.from(slides).indexOf(nextSlide);
-    radioButtons.forEach((button, index) => {
-      button.checked = index === currentIndexRadio;
-    });
-  }
-  }
-}
-
-nextSlide();
-
+// Écouteur d'événement pour charger le carrousel après le chargement de la page
+window.addEventListener('load', initializeCarousel);
